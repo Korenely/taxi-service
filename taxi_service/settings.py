d30161695932fa1aed7fe3a46c81b863b7119659
Dorius <kornelydorius@gmail.com>
2022-07-08 13:46:29 +0300

    Updated static

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -133,6 +134,8 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 STATICFILES_DIRS = (BASE_DIR / "static",)
+
+STATIC_ROOT = "staticfiles/"
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
